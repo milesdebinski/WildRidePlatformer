13a3987931a2208d5ff6aeff299f9e4b26fc2c6c
Miles Debinski <miles.debinski@gmail.com>
2021-12-18 18:14:29 +0000

    add speed booster

--- WildRidePlatformerGame/Assets/Scripts/PlayerController.cs
+++ WildRidePlatformerGame/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+  [SerializeField] float torqueAmount = 3f;
+  [SerializeField] float boostSpeed = 21f;
+  [SerializeField] float baseSpeed = 14f;
   Rigidbody2D rb2d;
-  [SerializeField] float torqueAmount = 1f;
+  SurfaceEffector2D surfaceEffector2D;
 
 
   // Start is called before the first frame update
   void Start()
   {
     rb2d = GetComponent<Rigidbody2D>();
+    surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
 
   }
 
   // Update is called once per frame
   void Update()
+  {
+    RotatePlayer();
+    RespondToBoost();
+  }
+
+  void RespondToBoost()
+  {
+    if (Input.GetKey(KeyCode.UpArrow))
+    {
+      surfaceEffector2D.speed = boostSpeed;
+    }
+    else
+    {
+      surfaceEffector2D.speed = baseSpeed;
+    }
+
+    // if we push up then speed up
+    // ohterwise stay at normal speed
+  }
+
+  void RotatePlayer()
   {
     if (Input.GetKey(KeyCode.LeftArrow))
     {
