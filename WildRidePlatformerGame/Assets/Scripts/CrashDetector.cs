a80d5d41ebe8339ecc78ec674eaebee0412edf1b
Miles Debinski <miles.debinski@gmail.com>
2021-12-18 21:39:48 +0000

    prevent double kill

--- WildRidePlatformerGame/Assets/Scripts/CrashDetector.cs
+++ WildRidePlatformerGame/Assets/Scripts/CrashDetector.cs
@@ -5,17 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-  [SerializeField] float loadDelay = 0.5f;
+  [SerializeField] float loadDelay = 1f;
   [SerializeField] ParticleSystem crashEffect;
   [SerializeField] AudioClip crashSFX;
+  bool hasCrashed = false;
 
 
-  private void OnTriggerEnter2D(Collider2D other)
+  void OnTriggerEnter2D(Collider2D other)
   {
-    if (other.tag == "Ground")
+    if (other.tag == "Ground" && !hasCrashed)
     {
+      hasCrashed = true;
       Debug.Log("Ouch!! it hurts!");
       crashEffect.Play();
+      FindObjectOfType<PlayerController>().DisableControls();
       GetComponent<AudioSource>().PlayOneShot(crashSFX);
       Invoke("ReloadScene", loadDelay);
     }
